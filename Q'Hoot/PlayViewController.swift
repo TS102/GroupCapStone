218dfdd7d36895868547539ba6554ebdd781fe1b
Steven Newman <stevennewman@Stevens-MacBook-Pro.local>
2023-04-18 09:11:25 -0600

    First Version of the UI storyboard

---
+++ Q'Hoot/PlayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlayViewController.swift
+//  Q'Hoot
+//
+//  Created by Steven Newman on 4/17/23.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
